26bf9876a8adf824fd065bc6b0220a1ac4e62e9a
Tim Pizey <timp@paneris.org>
2011-05-10 23:02:46 +0100

    First commit

---
+++ src/test/java/org/cggh/casutils/test/CasProtectedResourceDownloaderTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.cggh.casutils.test;
+
+import org.cggh.casutils.CasProtectedResourceDownloader;
+
+import junit.framework.TestCase;
+
+/**
+ * @author timp
+ *
+ */
+public class CasProtectedResourceDownloaderTest extends TestCase {
+
+  /**
+   * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#download(java.lang.String)}.
+   */
+  public void testDownload() {
+
+  }
+
+  /**
+   * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#downloadUrlToFile(java.lang.String, java.io.File)}.
+   */
+  public void testDownloadUrlToFile() {
+  }
+
+  /**
+   * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#getServiceTicket(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+   */
+  public void testGetServiceTicket() {
+  }
+
+}
