9e0d8ae5d6b87e673d30f2af0eadf6f576cd7840
Tim Pizey <timp@paneris.org>
2011-05-10 23:27:22 +0100

    Initial tests

--- src/test/java/org/cggh/casutils/test/CasProtectedResourceDownloaderTest.java
+++ src/test/java/org/cggh/casutils/test/CasProtectedResourceDownloaderTest.java
@@ -3,6 +3,8 @@
  */
 package org.cggh.casutils.test;
 
+import java.io.File;
+
 import org.cggh.casutils.CasProtectedResourceDownloader;
 
 import junit.framework.TestCase;
@@ -16,20 +18,15 @@ public class CasProtectedResourceDownloaderTest extends TestCase {
   /**
    * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#download(java.lang.String)}.
    */
-  public void testDownload() {
-
+  public void testDownload() throws Exception {
+    assertEquals("file:///tmp/ZAZBM", CasProtectedResourceDownloader.download("http://cloud1.cggh.org/repository/service/content/studies/ZAZBM"));
   }
 
   /**
    * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#downloadUrlToFile(java.lang.String, java.io.File)}.
    */
-  public void testDownloadUrlToFile() {
-  }
-
-  /**
-   * Test method for {@link org.cggh.casutils.CasProtectedResourceDownloader#getServiceTicket(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
-   */
-  public void testGetServiceTicket() {
+  public void testDownloadUrlToFile() throws Exception {
+    assertEquals(200, CasProtectedResourceDownloader.downloadUrlToFile("http://cloud1.cggh.org/repository/service/content/studies/ZAZBM", new File("t.tmp")));
   }
 
 }
