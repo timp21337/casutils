d42d7869cec077a19f4908549441e6a511340433
timp21337 <timp@paneris.org>
2011-05-20 18:57:57 +0100

    Refactored to test three servers

--- src/test/java/org/cggh/casutils/test/HttpClientTest.java
+++ src/test/java/org/cggh/casutils/test/HttpClientTest.java
@@ -14,33 +14,71 @@ import javax.net.ssl.SSLSocketFactory;
 
 import junit.framework.TestCase;
 /**
+ * A set of tests to prove that SSL is working correctly 
  * 
  * @see http://hc.apache.org/httpclient-legacy/sslguide.html
  *
  */
 public class HttpClientTest extends TestCase {
       
-  public static final String TARGET_HTTPS_SERVER = "www.verisign.com"; 
-  //public static final String TARGET_HTTPS_SERVER = "cloud1.cggh.org"; 
   public static final int    TARGET_HTTPS_PORT   = 443; 
-      
+  public static final String GET = "GET";
+  public static final String POST = "POST";
+  
+  
   public HttpClientTest(String name) {
     super(name);
   }
   
-  public static void main(String[] args) throws Exception {
-    testGet();
-    testPost();
+
+  public  static void testGetVerisign() throws IOException, UnknownHostException, UnsupportedEncodingException {
+    String httpsServer = "www.verisign.com";
+    String output = requestService(GET,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 200 OK", output.substring(0,15));
+    
+    output = requestService(POST,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 200 OK", output.substring(0,15));
   }
 
-  public  static void testGet() throws IOException, UnknownHostException, UnsupportedEncodingException {
+  public  static void testCloud1() throws IOException, UnknownHostException, UnsupportedEncodingException {    
+    String httpsServer = "cloud1.cggh.org";
+    String output = requestService(GET,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 302 Fo", output.substring(0,15));
+    
+    output = requestService(POST,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 302 Fo", output.substring(0,15));
+  }
+
+  public  static void testWwarn() throws IOException, UnknownHostException, UnsupportedEncodingException {
+    String httpsServer = "www.wwarn.org";
+    String output = requestService(GET,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 302 Fo", output.substring(0,15));
+    
+    output = requestService(POST,httpsServer );
+    
+    assertEquals("Have you setup trust certificates for " + httpsServer + "?\n" + 
+        output, "HTTP/1.1 302 Fo", output.substring(0,15));
+  }
+
+  private static String  requestService(String method, String httpsServer) throws IOException, UnknownHostException, UnsupportedEncodingException {
     Socket socket = SSLSocketFactory.getDefault().
-       createSocket(TARGET_HTTPS_SERVER, TARGET_HTTPS_PORT);
+       createSocket(httpsServer, TARGET_HTTPS_PORT);
+    StringBuffer output;
      try {
        Writer out = new OutputStreamWriter(
           socket.getOutputStream(), "ISO-8859-1");
-       out.write("GET / HTTP/1.1\r\n");  
-       out.write("Host: " + TARGET_HTTPS_SERVER + ":" + 
+       out.write(method + " / HTTP/1.1\r\n");  
+       out.write("Host: " + httpsServer + ":" + 
            TARGET_HTTPS_PORT + "\r\n");  
        out.write("Agent: SSL-TEST\r\n");  
        out.write("\r\n");  
@@ -48,40 +86,15 @@ public class HttpClientTest extends TestCase {
        BufferedReader in = new BufferedReader(
           new InputStreamReader(socket.getInputStream(), "ISO-8859-1"));
        String line = null;
-       StringBuffer output = new StringBuffer();
+       output = new StringBuffer();
        while ((line = in.readLine()) != null) {
          output.append(line);
          output.append("\n");
        }
-       assertEquals("Have you setup trust certificates?", "HTTP/1.1 200 OK", output.substring(0,15));
      } finally {
        socket.close(); 
      }
+     return output.toString();
   }
   
-  public  static void testPost() throws IOException, UnknownHostException, UnsupportedEncodingException {
-    Socket socket = SSLSocketFactory.getDefault().
-       createSocket(TARGET_HTTPS_SERVER, TARGET_HTTPS_PORT);
-     try {
-       Writer out = new OutputStreamWriter(
-          socket.getOutputStream(), "ISO-8859-1");
-       out.write("POST / HTTP/1.1\r\n");  
-       out.write("Host: " + TARGET_HTTPS_SERVER + ":" + 
-           TARGET_HTTPS_PORT + "\r\n");  
-       out.write("Agent: SSL-TEST\r\n");  
-       out.write("\r\n");  
-       out.flush();  
-       BufferedReader in = new BufferedReader(
-          new InputStreamReader(socket.getInputStream(), "ISO-8859-1"));
-       String line = null;
-       StringBuffer output = new StringBuffer();
-       while ((line = in.readLine()) != null) {
-         output.append(line);
-         output.append("\n");
-       }
-       assertEquals("Have you setup trust certificates?", "HTTP/1.1 200 OK", output.substring(0,15));
-     } finally {
-       socket.close(); 
-     }
-  }
 }
